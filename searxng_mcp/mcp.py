153dc7ee5bacc7a696635c07096f1f0856b06e45
KnucklesSG1 <knucklessg1@gmail.com>
2026-03-09 00:16:44 -0500

    Bump version: 0.1.37 → 0.1.38

--- searxng_mcp/mcp.py
+++ searxng_mcp/mcp.py
@@ -20,7 +20,7 @@ from agent_utilities.mcp_utilities import (
     config,
 )
 
-__version__ = "0.1.37"
+__version__ = "0.1.38"
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
